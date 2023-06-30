2f780d790ba3fcbe4df2646320247986dd784e2c
hamza0142 <you@example.com>
2023-06-29 17:48:35 -0700

    forth1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src) 
 {
 	int i;
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
 	j = 0;
-	while (src[j] != '\0')
+	while (dest[j] != '\0')
 	{
-		dest[i] = src[j];
-		i++;
 		j++;
 	}
-
-	dest[i] = '\0';
+	while (src[i] != '\0')
+	{
+		dest[i + j] = src[i];
+		i++;
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
